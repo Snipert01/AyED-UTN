86fc6452fae312db5f01a782fa37052c80afd0d9
Snipert01 <33577925+Snipert01@users.noreply.github.com>
2022-06-10 12:49:01 -0300

    .

---
+++ Guia03/Ej2/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main()
+{
+    float numero;
+    int contador = 0;
+    double menor = 0;
+    ifstream archilec;
+    archilec.open("02entrada.txt");
+    if (!archilec)
+    {
+        cout << "No se pudo abrir el archivo" << endl;
+        return 1;
+    }
+    while (archilec >> numero)
+    {
+        cout << "Ingresado: " << numero << endl;
+        contador++;
+        if (contador == 1)
+        {
+            menor = numero;
+        }
+        else
+        {
+            {
+                if (numero < menor)
+            
+                menor = numero;
+            }
+        }        
+    }
+    cout << "El menor es: " << menor << endl;
+    cout << "Cantidad de datos ingresados: " << contador << endl;
+    archilec.close();
+    return 0;
+}
