698f8e0de6046a54be3a152129a827effafa57f0
Snipert01 <33577925+Snipert01@users.noreply.github.com>
2022-06-18 19:05:31 -0300

    commit

--- Guia03/Ej5/main.cpp
+++ Guia03/Ej5/main.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+int main()
+{
+    ofstream archiesc;
+    archiesc.open("texto.txt");
+    if (!archiesc)
+    {
+        cout << "No se pudo abrir el archivo" << endl;
+        return 1;
+    }
+
+    cout << "Ingresar palabras donde haya como máximo 80 caracteres por renglón: " << endl;
+    string linea;
+    while (getline(cin, linea))
+    {
+        archiesc << linea << endl;
+    }
+    linea.size();
+    if (linea.size() > 80)
+    {
+        cout << "La palabra es muy larga" << endl;
+    }
+    archiesc.close();
+    return 0;
+}
